3eb17c9cbf9284d639442835af656e684ced7beb
Antony <akb@akb.me.uk>
2014-02-12 16:42:26 +0000

    Added client page (obviously it'd be better if this only appeared when you clicked the "Client" button, but baby-steps...)

--- sys/htdocs/client.js
+++ sys/htdocs/client.js
@@ -1,5 +1,38 @@
 $(document).ready(function()
 {
-    $("#radio").buttonset();
+    $('#radio').buttonset();
+
+    $.getJSON('selectSql?table=Clients', function(data, textStatus, jqXHR)
+    {
+        var sHtml = '';
+    
+        sHtml += '<tr>\n';
+
+        // Those fieldnames are still going to have to come from somewhere if there are zero records
+        for(var fld in data[0])
+        {
+            sHtml += '<th>' + fld + '</th>\n';
+        }
+
+        sHtml += '</tr>\n';
+
+        for(var n in data)
+        {
+            var rs = data[n];
+
+            sHtml += '<tr>\n';
+            
+            for(var fld in rs)
+            {
+                sHtml += '<td>' + rs[fld] + '</td>\n';
+            }
+
+            sHtml += '</tr>\n';
+        }
+        
+        $('#tableClients').html(sHtml);
+        
+        $('tr:even').addClass('trAlt');
+    });
 });
 
