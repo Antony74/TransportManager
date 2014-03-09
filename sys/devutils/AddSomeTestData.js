424afb9c1701990491f184479ca915deac16b343
Antony <akb@akb.me.uk>
2014-03-09 20:21:05 +0000

    Got some test data into the system

--- sys/devutils/AddSomeTestData.js
+++ sys/devutils/AddSomeTestData.js
@@ -6,6 +6,19 @@ var Faker = require('Faker');
 function addTestClient(rs)
 {
     rs.AddNew();
+    rs.Fields("Title").Value = "";
+    rs.Fields("Firstname").Value = Faker.Name.firstName();
+    rs.Fields("Initial").Value = Faker.Name.firstName().substr(0,1);
+    rs.Fields("Surname").Value = Faker.Name.lastName();
+    rs.Fields("AddressLine1").Value = Faker.Address.streetAddress();
+    rs.Fields("AddressLine2").Value = "";
+    rs.Fields("Town").Value = "Neverville";
+    rs.Fields("Postcode").Value = "";
+    rs.Fields("HomeNumber").Value = Faker.PhoneNumber.phoneNumber().substr(0,13);
+    rs.Fields("MobileNumber").Value = "";
+    rs.Fields("EmailAddress").Value = "";
+    rs.Fields("Notes").Value = "";
+    rs.Fields("IsActive").Value = true;
     rs.Update();
 }
 
@@ -16,7 +29,7 @@ platform.ensureDatabaseIsReady(function()
     var rsClients = jet.createRecordset();
     rsClients.Open("Clients", db, jet.adOpenStatic, jet.adLockOptimistic, jet.adCmdTableDirect);
     
-    for (var n = 0; n < 1; ++n)
+    for (var n = 0; n < 8; ++n)
     {
         addTestClient(rsClients);
     }
