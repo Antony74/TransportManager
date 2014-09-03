c7df734b2bd36990bd13ba42c9078c601516a411
Antony74 <akb@akb.me.uk>
2014-09-03 18:51:23 +0100

    I have the TransportManager server console coming up within PowerShell.

--- sys/wintaskbar/TransportManager.ps1
+++ sys/wintaskbar/TransportManager.ps1
@@ -4,8 +4,54 @@
 
 Add-Type -AssemblyName System.Windows.Forms;
 
-$objNotifyIcon = New-Object System.Windows.Forms.NotifyIcon;
+$oTaskbarIcon = New-Object System.Windows.Forms.NotifyIcon;
 
-$objNotifyIcon.Icon = "..\htdocs\icons\Car.ico";
-$objNotifyIcon.Visible = $True;
+$oTaskbarIcon.Icon = "..\htdocs\icons\Car.ico";
+$oTaskbarIcon.Visible = $True;
+
+$sCmd = [system.io.file]::ReadAllText("..\server\TransportManager.bat");
+
+$arrWords = $sCmd.Split(" ");
+
+$oInfo = New-Object System.Diagnostics.ProcessStartInfo;
+$oInfo.FileName  = $arrWords[0].Trim("@");
+$oInfo.Arguments = $arrWords[1].Trim();
+$oInfo.UseShellExecute = $False;
+$oInfo.RedirectStandardOutput = $True;
+$oInfo.RedirectStandardError = $True;
+
+$oProcess = New-Object System.Diagnostics.Process;
+$oProcess.StartInfo = $oInfo;
+
+[Void]$oProcess.Start();
+
+$bDone = $False;
+
+while (!$bDone)
+{
+	$char = $oProcess.StandardOutput.Read();
+
+	if ($char -eq -1)
+	{
+		$char = $oProcess.StandardError.Read();
+	}
+
+	if ($char -eq -1)
+	{
+		if ($oProcess.HasExited)
+		{
+			$bDone = $True;
+		}
+		else
+		{
+			Wait-Event 1;
+		}
+	}
+	else
+	{
+		Write-Host -NoNewline "".PadLeft(1, $char);
+	}
+}
+
+$oTaskbarIcon.Visible = $False;
 
