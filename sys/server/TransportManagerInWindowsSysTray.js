336b7e0a052ec81a07f72ba34d76a46bc1faa699
Antony74 <akb@akb.me.uk>
2014-11-30 13:17:06 +0000

    Sketched out what I want the full set of parameters for this SysTray thing to look like.

--- sys/server/TransportManagerInWindowsSysTray.js
+++ sys/server/TransportManagerInWindowsSysTray.js
@@ -1,8 +1,29 @@
 
+var spawn = require('child_process').spawn;
+
 require('winsystraywrapper').run(
 {
-    'title': 'Transport Manager',
-    'icon' : __dirname + '/../htdocs/icons/Car.ico'
+    'title'     : 'Transport Manager',
+    'icon'      : __dirname + '/../htdocs/icons/Car.ico',
+    'exe'       : process.execPath,
+    'arguments' : __dirname + '\TransportManager.js',
+    'menu'      : [
+                    {
+                        'caption'  : 'Transport Manager',
+                        'function' : function()
+                                     {
+                                        spawn('cmd.exe', ['/c', 'start', 'http://localhost:8080']);
+                                     }
+                    },
+                    {
+                        'caption'  : 'Server log',
+                        'function' : this.showLog
+                    },
+                    {
+                        'caption'  : 'Stop',
+                        'function' : this.stop
+                    }
+                  ]
 });
 
 
