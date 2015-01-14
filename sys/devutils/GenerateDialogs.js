074a0c15c0e5a85bc6dba32da8828dec88f7fadc
Antony74 <akb@akb.me.uk>
2015-01-14 21:41:16 +0000

    GenerateDialogs script now produces useable html... useable dialog are still a little way off.

--- sys/devutils/GenerateDialogs.js
+++ sys/devutils/GenerateDialogs.js
@@ -1,14 +1,78 @@
 ///<reference path='../interface/node.d.ts' />
 
+var fs = require('fs');
 var dface = require('../server/node_modules/dface');
 var oTables = require('./Schema.js').getTables();
-var arrTables = [];
+
+var out = fs.createWriteStream(__dirname + '/../htdocs/raw/dialogs.html');
+
+out.write(generateHeader());
 
 for (var sTablename in oTables)
 {
-    generateDialog(sTablename);
+    out.write(generateButton(sTablename));
 }
 
+out.write('\r\n');
+
+for (var sTablename in oTables)
+{
+    out.write(generateDialog(sTablename));
+}
+
+out.write(generateFooter());
+
+out.end();
+
+//
+// generateHeader
+//
+function generateHeader()
+{
+    var s = '<!doctype html>\r\n';
+    s    += '<html lang="en">\r\n';
+    s    += '<head>\r\n';
+    s    += '    <title>Transport Manager Dialogs</title>\r\n';
+    s    += '    <link rel="stylesheet" href="../ui-lightness/jquery-ui-1.10.3.custom.css">\r\n';
+    s    += '    <link rel="stylesheet" href="../index.css">\r\n';
+    s    += '    <link rel="icon" type="image/png" href="../icons/Car.png">\r\n';
+    s    += '    <script src="../jquery-1.11.1.min.js"></script>\r\n';
+    s    += '    <script src="../jquery-ui-1.10.3.custom.min.js"></script>\r\n';
+    s    += '</head>\r\n';
+    s    += '<body>\r\n';
+    s    += '\r\n';
+    return s;
+}
+
+//
+// generateFooter
+//
+function generateFooter()
+{
+    var s = '\r\n';
+    s    += '</body>\r\n';
+    s    += '</html>\r\n';
+    return s;
+}
+
+//
+// generateButton
+//
+function generateButton(sTablename)
+{
+    var s = '<button id="' + sTablename + '">' + sTablename + '</button>\r\n';
+    s    += '<script>\r\n';
+    s    += "    $('#" + sTablename + "').click(function()\r\n";
+    s    += "    {\r\n";
+    s    += "        $('#dlg" + sTablename + "').dialog({modal: true, autoOpen: true, width: 400});\r\n";
+    s    += "    });\r\n";
+    s    += '</script>\r\n';
+    return s;
+}
+
+//
+// generateDialog
+//
 function generateDialog(sTablename)
 {
 
@@ -43,6 +107,6 @@ function generateDialog(sTablename)
     sForm     += '    </form>\r\n';
     sForm     += '</div>\r\n';
 
-    console.log(sForm);
+    return sForm;
 }
 
